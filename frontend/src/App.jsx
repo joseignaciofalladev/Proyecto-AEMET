fc3d7ce2851e52b381990bd2496afad1cacd2c7f
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-06 17:22:32 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,85 @@
+import { useState } from "react";
+import { getWeather } from "./services/weatherService";
+
 function App() {
+
+  // Estados necesarios
+  const [codigo, setCodigo] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async () => {
+
+    // validacion bosica: evitar busquedas vacias
+    if (!codigo.trim()) {
+      setError("Introduce un código");
+      return;
+    }
+
+    try {
+
+      // para activar el loading y limpiar errores anteriores
+      setLoading(true);
+      setError("");
+
+      // llamada al backend (fetch encapsulado en service)
+      const data = await getWeather(codigo);
+
+      // se guarda solo los datos necesarios del data.data viene del backend
+      setWeather(data.data);
+
+    } catch (err) {
+
+      // pori falla la peticion
+      setError("Error al obtener datos");
+      setWeather(null);
+    } finally {
+
+      // siempre ejecutar aunque de error
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <h1>App Meteorológica</h1>
+      <h1>Busqueda por municipio</h1>
+
+      {/* priemr input */}
+      <input
+        type="text"
+        placeholder="Código municipio (ej: 28079)"
+        value={codigo}
+        onChange={(e) => setCodigo(e.target.value)}
+      />
+
+      <button onClick={handleSearch}>Buscar</button>
+
+      {/* mensajes de carga y error */}
+      {loading && <p>Cargando...</p>}
+      {error && <p>{error}</p>}
+
+      {/* muestreo del weather, aun no siendo componente para ver si funciona la conexion end a end */}
+      {weather && (
+        <div>
+          <h2>{weather.municipio}</h2>
+          <p>Provincia: {weather.provincia}</p>
+          <p>Fecha: {weather.fecha}</p>
+
+          <p>Temp max: {weather.temperatura.max}°C</p>
+          <p>Temp min: {weather.temperatura.min}°C</p>
+
+          <p>{weather.estadoCielo}</p>
+          <p>Lluvia: {weather.lluvia}%</p>
+          <p>Viento: {weather.viento} km/h</p>
+        </div>
+      )}
     </div>
   );
+
+  // para resolver unos errores
+  console.log(data);
+  console.log(weather);
 }
 
 export default App;
