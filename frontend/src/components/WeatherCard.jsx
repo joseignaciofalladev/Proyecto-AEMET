3cac2d82dfce01ced3c7fb8c6431b9369aa1cdb9
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-06 22:53:39 +0200

    Add files via upload

---
+++ frontend/src/components/WeatherCard.jsx
@@ -0,0 +1,25 @@
+function WeatherCard({ weather }) {
+  return (
+    <div style={{
+      marginTop: "20px",
+      padding: "20px",
+      background: "white",
+      borderRadius: "10px",
+      boxShadow: "0 0 10px rgba(0,0,0,0.1)"
+    }}>
+      <h2>{weather.municipio}</h2>
+      <p>{weather.provincia}</p>
+
+      <p><strong>Fecha:</strong> {weather.fecha}</p>
+
+      <p>🌡️ Max: {weather.temperatura.max}°C</p>
+      <p>🌡️ Min: {weather.temperatura.min}°C</p>
+
+      <p>☁️ {weather.estadoCielo}</p>
+      <p>🌧️ {weather.lluvia}% lluvia</p>
+      <p>💨 {weather.viento} km/h</p>
+    </div>
+  );
+}
+
+export default WeatherCard;
