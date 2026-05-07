bac41686386798a2388666fb26b05052ba95c622
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-07 14:23:54 +0200

    Add files via upload

---
+++ frontend/src/data/municipios.js
@@ -0,0 +1,8 @@
+export const municipios = [
+  { nombre: "Madrid", codigo: "28079" },
+  { nombre: "Barcelona", codigo: "08019" },
+  { nombre: "Valencia", codigo: "46250" },
+  { nombre: "Sevilla", codigo: "41091" },
+  { nombre: "Zaragoza", codigo: "50297" },
+  { nombre: "Murcia", codigo: "30030" },
+];
