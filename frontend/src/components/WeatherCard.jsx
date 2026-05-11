8f02be8d268c5ba519d4cf435d8f385c012eb0af
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-11 22:52:10 +0200

    Update WeatherCard.jsx

--- frontend/src/components/WeatherCard.jsx
+++ frontend/src/components/WeatherCard.jsx
@@ -1,16 +1,31 @@
 function WeatherCard({ weather }) {
+  const hoy = weather.dias[0];
   return (
     <div className="weather-card">
-      <h2>{weather.municipio}</h2>
 
-      <div className="temp-section">
-        <h1>{weather.temperatura.max}°C</h1>
-        <p>Min: {weather.temperatura.min}°C</p>
+      <h2>{weather.municipio}</h2>
+      <p>{weather.provincia}</p>
+
+      <div className="today-weather">
+        <h1>{hoy.temperatura.max}°C</h1>
+        <p>Min: {hoy.temperatura.min}°C</p>
+        <p>{hoy.estadoCielo}</p>
+        <p>{hoy.lluvia}%</p>
+        <p>{hoy.viento} km/h</p>
       </div>
 
-      <p>{weather.estadoCielo}</p>
-      <p>Lluvia: {weather.lluvia}%</p>
-      <p>Viento: {weather.viento} km/h</p>
+      <div className="forecast-container">
+
+        {weather.dias.slice(1).map((dia, index) => (
+
+          <div key={index} className="forecast-card">
+            <h4>{new Date(dia.fecha).toLocaleDateString()}</h4>
+            <p>{dia.temperatura.max}°</p>
+            <p>{dia.temperatura.min}°</p>
+            <p>{dia.lluvia}%</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
