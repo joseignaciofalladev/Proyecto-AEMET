4335cf08dd4b4c77406eed9faaf80689f1fe08ef
José Ignacio Falla García <joseignacio.falla67@gmail.com>
2026-05-17 20:35:51 +0200

    Create header.jsx

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="main-header">
+      <div className="header-overlay">
+        <h1>Proyecto AEMET</h1>
+        <p>Predicción meteorológica de municipios de España</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
